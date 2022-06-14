bde9e7c896da96112f6ca70490692699745d746c
Nilufar1026 <71069308+Nilufar1026@users.noreply.github.com>
2022-06-14 10:14:48 +0200

    fix education page

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -6,28 +6,20 @@ const Education = () => {
     <div id="education">
         <h2 className="heading">Education</h2>
         <div className="education-block">
-            <h3>University of Ottawa</h3>
-            <span className="education-date">Sept 2016 - Sept 2017</span>
-            <h4>Bachelor of Science in Computer Science</h4>
+            <h3>IT-Högskolan Stockholm</h3>
+            <span className="education-date">Aug 2020 - June 2022</span>
+            <h4>JavaScript developer</h4>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.
+            Course including HTML CSS, Native JavaScript, Graphic design and UX design, JavaScript with framework (vue.js), Design principle (test), Agile development, Software life cycle management, Crossplatform development (React native, expo).
             </p>
         </div>
         <div className="education-block">
-            <h3>University of Ottawa</h3>
-            <span className="education-date">Sept 2016 - Sept 2017</span>
-            <h4>Bachelor of Science in Computer Science</h4>
-            <ul>
-                <li>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </li>
-                <li>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </li>
-                <li>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </li>
-            </ul>
+            <h3>Geneva Business School </h3>
+            <span className="education-date">Sept 2015 - Dec 2017</span>
+            <h4>International management</h4>
+            <p>
+             It is MBA program as international management.
+            </p>
         </div>
     </div>
   );
